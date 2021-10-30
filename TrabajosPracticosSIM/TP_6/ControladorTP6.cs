e5524bee69e5752ee2da7ee600ae74fa18701a1f
Javier <javierbelbruno@gmail.com>
2021-10-29 23:27:34 -0300

    Setup inicial, Pantalla Inicial , Pantalla Enunciado lista

---
+++ TrabajosPracticosSIM/TP_6/ControladorTP6.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrabajosPracticosSIM.TP_6.InterfacesDeUsuario;
+
+namespace TrabajosPracticosSIM.TP_6
+{
+    public class ControladorTP6 : ApplicationContext
+    {
+        //Instancia Unica - Patron Singleton
+        private static readonly ControladorTP6 _instance = new ControladorTP6();
+        //Lista de Vistas / Pantallas que controla el ControladorTP5
+        private List<Form> Views = new List<Form>();
+
+        //Constructor Privado.
+        private ControladorTP6()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+
+        // Devolver instancia estática única.
+        public static ControladorTP6 GetInstance() { return _instance; }
+
+        public void Start()
+        {
+            IniciarFuncionalidad();
+            Application.Run(this);
+        }
+
+        public void IniciarFuncionalidad()
+        {
+            HabilitarPantallaPrincipal();
+        }
+
+        public void HabilitarPantallaPrincipal()
+        {
+            CreateView(new Frm_TP6_Principal());
+        }
+        //Crear una ventana
+        public void CreateView(Form frm)
+        {
+            //Pregunto si ya existe este Tipo de Pantalla, si no existe la creo.
+            bool EstaRepetidaLaVista = false;
+            foreach (var v in Views)
+            {
+                if (frm.GetType() == v.GetType())
+                    EstaRepetidaLaVista = true;
+            }
+            if (!EstaRepetidaLaVista)
+            {
+                Views.Add(frm);
+                frm.FormClosed += FormClosed;
+                frm.Show();
+            }
+        }
+        ///Evento Cerrar un Form
+        private void FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //Remover Pantalla de la lista de Pantallas.
+            Views.Remove(sender as Form);
+            // NOTE: Terminar programa si no quedan mas Vistas/Forms o si se está cerrando la ventana principal.
+            if (Views.Count == 0 || sender.GetType() == typeof(Frm_TP6_Principal)) Exit();
+        }
+
+        //Finalizar Programa
+        public void Exit()
+        {
+            this.ExitThread();
+        }
+        /// <summary>
+        /// Pantallas
+        /// </summary>
+        public void OpcionPantallaEnunciado()
+        {
+            CreateView(new Frm_TP6_Enunciado());
+        }
+        public void OpcionPuntoAE()
+        {
+            CreateView(new Frm_TP6_PantallaPuntoAE());
+        }
+        public void OpcionPuntoF()
+        {
+            CreateView(new Frm_TP6_PantallaPuntoF());
+        }
+    }
+}
---
+++ TrabajosPracticosSIM/TP_6/InterfacesDeUsuario/Frm_TP6_Principal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TrabajosPracticosSIM.TP_6.InterfacesDeUsuario
+{
+    public partial class Frm_TP6_Principal : Form
+    {
+        public Frm_TP6_Principal()
+        {
+            InitializeComponent();
+        }
+
+        //Inicio Caso de Uso Punto A 
+        private void btn_punto_a_e_Click(object sender, EventArgs e)
+        {
+            ControladorTP6.GetInstance().OpcionPuntoAE();
+        }
+
+        private void btn_Enunciado_Click(object sender, EventArgs e)
+        {
+            ControladorTP6.GetInstance().OpcionPantallaEnunciado();
+        }
+
+        private void btn_punto_f_Click(object sender, EventArgs e)
+        {
+            ControladorTP6.GetInstance().OpcionPuntoF();
+        }
+    }
+}
