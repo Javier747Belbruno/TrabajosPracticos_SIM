ddfdc4701773b67aecf616d7bdb943edf90412b3
Javier <javierbelbruno@gmail.com>
2021-10-31 12:49:15 -0300

    Graficos Listos

---
+++ TrabajosPracticosSIM/TP_6/InterfacesDeUsuario/Frm_TP6_GraficoTipoT.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace TrabajosPracticosSIM.TP_6.InterfacesDeUsuario
+{
+    partial class Frm_TP6_GraficoTipoT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.grafico = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grafico
+            // 
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Calibri", 12F);
+            chartArea1.Name = "ChartArea1";
+            this.grafico.ChartAreas.Add(chartArea1);
+            this.grafico.Location = new System.Drawing.Point(12, 12);
+            this.grafico.Name = "grafico";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Blue;
+            series1.Legend = "Legend1";
+            series1.Name = "Euler";
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series2.Legend = "Legend1";
+            series2.Name = "RK";
+            this.grafico.Series.Add(series1);
+            this.grafico.Series.Add(series2);
+            this.grafico.Size = new System.Drawing.Size(1324, 747);
+            this.grafico.TabIndex = 0;
+            this.grafico.Text = "chart1";
+            title1.Font = new System.Drawing.Font("Calibri", 16F);
+            title1.Name = "Title1";
+            title1.Text = "Evolución del promedio de Tiempo de Ensamble";
+            this.grafico.Titles.Add(title1);
+            // 
+            // Frm_TP6_GraficoTipoT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 771);
+            this.Controls.Add(this.grafico);
+            this.Name = "Frm_TP6_GraficoTipoT";
+            this.Text = "Frm_TP4_PuntoD";
+            ((System.ComponentModel.ISupportInitialize)(this.grafico)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart grafico;
+    }
+}
